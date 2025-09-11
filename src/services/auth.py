8ec7f07f50bc66af83c844e1196f90d1faf2a751
pussy <pussy.com>
2025-09-11 16:37:15 +0300

    Picked out the high-level API for creating jwt and access tokens

---
+++ src/services/auth.py
@@ -0,0 +1,39 @@
+from src.core import security as auth_security, settings
+from src.scheme import UserResponse
+from datetime import timedelta
+from src.core.config import settings
+
+
+TOKEN_TYPE_FIELD = "type"
+ACCESS_TOKEN_TYPE = "access"
+REFRESH_TOKEN_TYPE = "refresh"
+
+
+def create_token(payload: dict, resfresh_token_expire_days: timedelta | None = None):
+    
+    token = auth_security.create_jwt(payload, expire_timedelta_days=resfresh_token_expire_days)
+    return token
+
+
+def create_access_token(user: UserResponse):
+    
+    payload = {
+        "sub": str(user.id),
+        "is_active": user.is_active,
+        "is_superuser": user.is_superuser,
+        TOKEN_TYPE_FIELD: ACCESS_TOKEN_TYPE,
+    }
+
+    return create_token(payload)
+
+
+def create_refresh_token(user: UserResponse):
+    payload = {
+        "sub": str(user.id),
+        TOKEN_TYPE_FIELD: REFRESH_TOKEN_TYPE,
+    }
+
+    return create_token(
+            payload=payload,    
+            resfresh_token_expire_days=timedelta(days=settings.auth_jwt.refresh_token_expire_days),   
+        )

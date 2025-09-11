b285d09f9d7ad339a5353c0a2ee887208d1e8cac
pussy <pussy.com>
2025-09-11 16:38:01 +0300

    Made changes for route <user>. Was made some refactoring
    Made changes for route <auth>. Was realized API for creating refresh token and give it to user

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter, Depends, Form, status
 from fastapi.security import HTTPBearer, OAuth2PasswordBearer
 from fastapi.exceptions import HTTPException
-from src.scheme.user import CreateUser, UserRegister
+from src.scheme.user import CreateUser, RegistrationUser, UserResponse
 from src.scheme.token import TokenInfo
 from src.core import security as auth_security, settings
+from src.repositories.user import UserServices, get_user_service
+from src.models.user import User
+from src.services.auth import create_access_token, create_refresh_token
+from pydantic import EmailStr
 
 
 router = APIRouter(tags=['authorisation'], prefix='/auth')
@@ -11,68 +15,71 @@ router = APIRouter(tags=['authorisation'], prefix='/auth')
 http_bearer = HTTPBearer()
 oauth_sheme = OAuth2PasswordBearer(tokenUrl="auth/login",)
 
-john = CreateUser(
-    username="John",
-    password=auth_security.hash_password("qwerty"),
-    email="john@example.com",
-    is_active=True
-)
-
-
-sam = CreateUser(
-    username="Sam",
-    password=auth_security.hash_password("secret"),
-    email="sam@example.com",
-    is_active=True 
-)
-
-users_db: dict[str, CreateUser] = {
-    john.username: john,
-    sam.username: sam,
-}
-
-
 
 
 def validate_auth_user(
-        username: str = Form(...),
+        email: EmailStr = Form(...),
        password: str = Form(...),
+       user_services : UserServices = Depends(get_user_service)
        ):
-    unauthed_exc = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid username or password")
+    unauthed_exc = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid email or password")
 
-    if not (user := users_db.get(username, None)):
+    user = user_services.get_user_by_email(email)
+    if not user:
         raise unauthed_exc
     
-    if not (auth_security.verify_password(password=password, hashed_password=user.password)):
+    if not (auth_security.verify_password(password=password, hashed_password=user.hashed_password)):
         raise unauthed_exc
     
-    return user
-
-
-@router.post('/login', response_model=TokenInfo)
-def login(user: CreateUser = Depends(validate_auth_user)):
-
-    payload = {
-        "sub": user.username,
-        "username": user.username,
-        "emial": user.email,
-    }
-
-    token = auth_security.create_jwt(payload)
-
-    return TokenInfo(
-        access_token=token,
-        token_type="Bearer"
+    return UserResponse(
+        id=user.id,          
+        username=user.username,
+        email=user.email,
+        is_active=user.is_active,
+        is_superuser=user.is_superuser
     )
 
 
-def validate_registration():
-    ...
+
+
+@router.post('/login', response_model=TokenInfo)
+def login(user: UserResponse = Depends(validate_auth_user)):
+
+    access_token = create_access_token(user)
+    refresh_token = create_refresh_token(user)
+
+    return TokenInfo(
+        access_token=access_token,
+        refresh_token=refresh_token,
+    )
+
+
+
+def validate_registration_user(user: RegistrationUser = Form(...)) -> CreateUser:
+    
+        if user.password1 != user.password2:
+             raise HTTPException(status_code=422, detail="Passwords not euqal")
+    
+        return CreateUser(
+                username=user.username,
+                email=user.email,
+                password=auth_security.hash_password(user.password1),
+                is_active=True
+                )
+
 
 @router.post('/registration')
-def registration(user: UserRegister= Depends(validate_registration)):
-    if user.password1 == user.password2:
-        users_db.update({user.username: user})
-        return {"succes": True, "status_code": 302, "redirect": "/login"} 
-    return {"success": False, "status_code": 422, "description": "Passwords not equal"}
+def registration(user: CreateUser= Depends(validate_registration_user), 
+        user_services: UserServices =Depends(get_user_service)) -> dict:
 
+        
+        #Try to create new user
+        result = user_services.create_user(user)
+        if not result:
+             raise HTTPException(
+                status_code=400, 
+                detail="User with this email address is alredy exist"
+            )
+        
+        return {"succes": True, "status_code": 302, "redirect": "/login"} 
+    
--- src/routes/user.py
+++ src/routes/user.py
@@ -4,8 +4,9 @@ from fastapi.security import HTTPAuthorizationCredentials
 from typing import Annotated
 from src.routes.auth import oauth_sheme, http_bearer
 from src.core import security as auth_security
-from src.scheme import TokenInfo, CreateUser
-from src.routes.auth import users_db
+from src.scheme import TokenInfo, UserResponse
+from src.repositories.user import UserServices, get_user_service
+
 from jwt import InvalidTokenError
 
 
@@ -13,46 +14,55 @@ router = APIRouter(tags=["users"], prefix='/users')
 
 
 
-def get_payload(token: str = Depends(oauth_sheme)):
-    try:
-        payload  = auth_security.decode_jwt(token=token)
-    except InvalidTokenError:
-        raise HTTPException(status_code=401, detail="invalid token error")
-
-    return payload
-
-
-# def get_payload(creds: Annotated[HTTPAuthorizationCredentials , Depends(http_bearer)]) -> dict:
-#     token = creds.credentials
-
+# def get_payload(token: str = Depends(oauth_sheme)):
 #     try:
 #         payload  = auth_security.decode_jwt(token=token)
-#     except InvalidTokenError:
-#         raise HTTPException(status_code=401, detail="invalid token error")
+#     except InvalidTokenError as e:
+#         raise HTTPException(status_code=401, detail=f"invalid token error {e}")
 
 #     return payload
 
 
-def get_authed_user(payload: dict = Depends(get_payload)) -> CreateUser:
+def get_payload(creds: Annotated[HTTPAuthorizationCredentials , Depends(http_bearer)]) -> dict:
+    token = creds.credentials
+
+    try:
+        payload  = auth_security.decode_jwt(token=token)
+    except InvalidTokenError as e:
+        raise HTTPException(status_code=401, detail=f"invalid token error {e}")
+
+    return payload
+
+
+def get_authed_user(payload: dict = Depends(get_payload),
+    user_services: UserServices=Depends(get_user_service)) -> UserResponse:
     
-    username = payload.get("sub")   
-    if not (user:=users_db.get(username)):
+    id = int(payload.get("sub"))   
+    if not (user:=user_services.get_user_by_id(id)):
         raise HTTPException(status_code=401, detail="token invalid(user not found)")
         
-    return user
+    return UserResponse(
+        id = user.id,
+        username=user.username,
+        email=user.email,
+        is_active=user.is_active,
+        is_superuser=user.is_superuser
+    )
     
 
-def get_active_user(user: CreateUser = Depends(get_authed_user)) -> CreateUser:
+def get_active_user(user: UserResponse = Depends(get_authed_user)) -> UserResponse:
     if user.is_active:
         return user
     return HTTPException(status_code=403, detail="user inactive")
 
 
 @router.get("/me")
-def get_user(user: CreateUser = Depends(get_active_user)) -> dict:
+def get_user(user: UserResponse = Depends(get_active_user)) -> dict:
     return {
         "username": user.username,
         "email": user.email,
+        "is_active": user.is_active,
+        "is_superuser": user.is_superuser,
     }
 
 
